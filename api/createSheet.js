8ba8fef607ff8b98ca738df5d105036487e6cf6c
Ruchikatak123 <takruchika789@gmail.com>
2025-11-22 18:28:08 -0800

    Update createSheet.js

--- api/createSheet.js
+++ api/createSheet.js
@@ -1,45 +1,23 @@
-const { google } = require('googleapis');
+const { google } = require("googleapis");
 
 module.exports = async (req, res) => {
   try {
-    const { access_token, refresh_token } = req.body;
+    const tokens = JSON.parse(req.body.tokens);
 
-    const oauth2Client = new google.auth.OAuth2(
-      process.env.CLIENT_ID,
-      process.env.CLIENT_SECRET,
-      process.env.REDIRECT_URI
-    );
+    const oauth2Client = new google.auth.OAuth2();
+    oauth2Client.setCredentials(tokens);
 
-    oauth2Client.setCredentials({ access_token, refresh_token });
+    const sheets = google.sheets({ version: "v4", auth: oauth2Client });
 
-    const drive = google.drive({ version: 'v3', auth: oauth2Client });
-
-    // 1. Create sheet
-    const file = await drive.files.create({
-      requestBody: {
-        name: 'Job Applications',
-        mimeType: 'application/vnd.google-apps.spreadsheet'
+    const newSheet = await sheets.spreadsheets.create({
+      resource: {
+        properties: { title: "My Job Applications" },
       }
     });
 
-    const sheetId = file.data.id;
-
-    // 2. Add columns
-    const sheets = google.sheets({ version: 'v4', auth: oauth2Client });
-
-    await sheets.spreadsheets.values.update({
-      spreadsheetId: sheetId,
-      range: "Sheet1!A1:F1",
-      valueInputOption: "RAW",
-      requestBody: {
-        values: [["Company", "Title", "Link", "Status", "Date", "Location"]]
-      }
-    });
-
-    // 3. Return URL
     return res.status(200).json({
-      sheetUrl: `https://docs.google.com/spreadsheets/d/${sheetId}/edit`,
-      sheetId
+      spreadsheetId: newSheet.data.spreadsheetId,
+      url: newSheet.data.spreadsheetUrl
     });
 
   } catch (err) {
