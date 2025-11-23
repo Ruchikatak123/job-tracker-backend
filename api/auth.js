d8013dd69ed754b6de4598aa88e6257ce7151527
Ruchikatak123 <takruchika789@gmail.com>
2025-11-22 18:22:56 -0800

    Refactor auth.js to use Express router

--- api/auth.js
+++ api/auth.js
@@ -1,26 +1,29 @@
-const { google } = require('googleapis');
+const express = require("express");
+const router = express.Router();
+const { google } = require("googleapis");
 
-module.exports = async (req, res) => {
-  try {
-    const oauth2Client = new google.auth.OAuth2(
-      process.env.CLIENT_ID,
-      process.env.CLIENT_SECRET,
-      process.env.REDIRECT_URI // MUST be /api/oauth2callback
-    );
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
 
-    const url = oauth2Client.generateAuthUrl({
-      access_type: 'offline',
-      scope: [
-        'https://www.googleapis.com/auth/spreadsheets',
-        'https://www.googleapis.com/auth/drive.file',
-        'https://www.googleapis.com/auth/script.projects'
-      ],
-      prompt: 'consent'
-    });
+const oauth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URI
+);
 
-    return res.status(200).json({ authUrl: url });
+router.get("/", (req, res) => {
+  const url = oauth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: [
+      "https://www.googleapis.com/auth/spreadsheets",
+      "https://www.googleapis.com/auth/drive.file",
+      "https://www.googleapis.com/auth/script.projects",
+    ],
+    prompt: "consent",
+  });
 
-  } catch (err) {
-    return res.status(500).json({ error: err.message });
-  }
-};
+  res.json({ authUrl: url });
+});
+
+module.exports = router;
